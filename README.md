31308fb318ba1c410951d77e9d8b4874edbb004a
Jeremy Grosser <jeremy@synack.me>
2022-04-05 17:48:06 -0700

    README and LICENSE

---
+++ README.md
@@ -0,0 +1,2 @@
+[Product page](https://shop.pimoroni.com/products/badger-2040)
+[Schematic](https://cdn.shopify.com/s/files/1/0174/1800/files/badger_2040_schematic.pdf)
--- src/uc8151.ads
+++ src/uc8151.ads
@@ -1,3 +1,8 @@
+--
+--  Copyright (C) 2022 Jeremy Grosser <jeremy@synack.me>
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
 with Ada.Unchecked_Conversion;
 with Memory_Mapped_Bitmap;
 with HAL.Framebuffer;
